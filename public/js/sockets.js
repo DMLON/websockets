62df563ab271a912e7a7e9bcab0ba191923a42f6
Damian Deza <damiandeza@gmail.com>
2021-08-30 17:42:41 -0300

    Added go to button

--- public/js/sockets.js
+++ public/js/sockets.js
@@ -15,7 +15,7 @@ const convertToTable = products => products.map( product =>
     <tr>
         <td>${product.title}</td>
         <td>$${product.price}</td>
-        <td><img src=${product.thumbnail}></td>
+        <td><img style="width=80px" src=${product.thumbnail}></td>
     </tr>
 `
 ).join(' ');
